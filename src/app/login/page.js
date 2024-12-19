fdc3c20fb84517c7b846c3f42872011f7308d40a
Natin Kumar <natinkumar1161@gmail.com>
2024-12-19 15:57:13 +0530

    routing normal

---
+++ src/app/login/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+const Login =()=>{
+    return (
+        <>
+        <h1>hello from login page</h1>
+        <Link href="/">go to home</Link>
+        </>
+    )
+}
+export default Login;
